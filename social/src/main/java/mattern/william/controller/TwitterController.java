8d1a3c876516ed0063918e78b0c6e0d7a543d496
wmattern0 <wm.m.mattern@gmail.com>
2017-03-11 10:56:41 -0500

    doScore method stubbed and configured

--- social/src/main/java/mattern/william/controller/TwitterController.java
+++ social/src/main/java/mattern/william/controller/TwitterController.java
@@ -1,5 +1,7 @@
 package mattern.william.controller;
 
+import mattern.william.entity.AnalyzedTweet;
+import mattern.william.service.WordParserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.social.twitter.api.Tweet;
@@ -21,8 +23,16 @@ public class TwitterController {
     @Autowired
     private Twitter twitter;
 
+    @Autowired
+    private WordParserService wordParserService;
+
     @RequestMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public List<Tweet> getTweets(){
-        return twitter.timelineOperations().getUserTimeline("realDonaldTrump");
+        //TODO get tweets, for each tweet assign a sentiment value, add value to tweet object
+        List<Tweet> singleTweet = twitter.timelineOperations().getUserTimeline("realDonaldTrump",1);
+        for (Tweet tweet:singleTweet){
+            wordParserService.doScore(tweet);
+        }
+        return twitter.timelineOperations().getUserTimeline("realDonaldTrump",1);
     }
 }
