6532a27477b8a333afe53c584f561c3ef0ce1cc9
wmattern0 <wm.m.mattern@gmail.com>
2017-03-17 18:29:27 -0400

    tests upgraded

--- social/src/main/java/mattern/william/controller/TwitterController.java
+++ social/src/main/java/mattern/william/controller/TwitterController.java
@@ -7,7 +7,6 @@ import org.springframework.http.MediaType;
 import org.springframework.social.twitter.api.Tweet;
 import org.springframework.social.twitter.api.Twitter;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
